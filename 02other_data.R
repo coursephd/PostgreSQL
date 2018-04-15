4f0820c207d84528a10ea9d275f6f523a301a672
Coursephd <mahajvi@yahoo.co.in>
2018-04-15 22:16:38 +0530

    Add files via upload

--- 02other_data.R
+++ 02other_data.R
@@ -26,8 +26,8 @@ patient_section_details.section_id = patient_section_values.option_id;
 
 ## Working: 
 create table base10_other11 as
-select patient_section_details.*, 
-patient_section_values.field_id, patient_section_values.option_id, patient_section_values.option_remarks
+select a.*, 
+b.field_id, b.option_id, b.option_remarks
 from patient_section_details as a, patient_section_values as b where a.section_detail_id=b.section_detail_id ;
 
 \copy base10_other11 TO 'd:/hospital_data/ProgresSQL/data_chk/base10_other11.csv' CSV HEADER DELIMITER ',';
@@ -35,6 +35,24 @@ from patient_section_details as a, patient_section_values as b where a.section_d
 \copy iaim.section_field_options TO 'd:/hospital_data/ProgresSQL/data_chk/section_field_options.csv' CSV HEADER DELIMITER ',';
 \copy iaim.section_field_desc TO 'd:/hospital_data/ProgresSQL/data_chk/section_field_desc.csv' CSV HEADER DELIMITER ',';
 \copy iaim.patient_consultation_field_values TO 'd:/hospital_data/ProgresSQL/data_chk/patient_consultation_field_values.csv' CSV HEADER DELIMITER ',';
+
+###############################
+Check the _orig dataset
+
+create table patient_section_details_orig as 
+select mr_no, patient_id, section_id, section_detail_id, section_item_id, item_type  
+from iaim.patient_section_details 
+order by mr_no, patient_id;
+
+## Working: 
+create table base10_other11_orig as
+select a.*, 
+b.field_id, b.option_id, b.option_remarks
+from patient_section_details_orig as a, patient_section_values as b where a.section_detail_id=b.section_detail_id ;
+
+\copy base10_other11_orig TO 'd:/hospital_data/ProgresSQL/data_chk/base10_other11_orig.csv' CSV HEADER DELIMITER ',';
+
+
 #============================================================
 
 library(data.table)
@@ -55,33 +73,63 @@ base01_other02 <- merge (x = base01_other02,
                        all.x = TRUE)
 
 # variable names
-section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_options.csv")
-section_field_options <- section_field_options[, c("option_id", "field_id", "display_order", "option_value"), with = FALSE]
+#section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_options.csv")
+section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_desc.csv")
+#section_field_options <- section_field_options[, c("option_id", "field_id", "display_order", "option_value"), with = FALSE]
 
 base01_other02 <- merge (x = base01_other02,
                          y = section_field_options,
-                         by = c("option_id", "field_id"),
+                         by = c("section_id", "field_id"), # by = c("option_id", "field_id")
                          all.x = TRUE)
+
 # Keep Unique records
-base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
+#base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
+base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id",  "field_id", "option_remarks", "section_title", "display_order", "field_name"), with =FALSE] )
 
 # Sort the data by patient and visits
-base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, option_id, field_id)]
+#base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, option_id, field_id)]
+base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, field_id)]
 
 # Do the clean-up where the variable name is not present
-base01_other03 <- base01_other02 [ nchar(option_value) > 0]
-base01_other04 <- base01_other02 [ option_id < 0]
+base01_other03 <- base01_other02 [ nchar(field_name) > 0]
+base01_other04 <- base01_other02 [ field_id < 0]
 
 # Create a counter variable for transpose
-base01_other030 <- base01_other03 [, subvis := 1:.N, by = .(mr_no, patient_id, section_id, option_id, field_id)]
+base01_other030 <- base01_other03 [, subvis := 1:.N, by = .(mr_no, patient_id, section_id, field_id, field_name)]
 
 # Transpose the data as per CRF pages
 base01_other030t <- dcast(data = base01_other030,
                           mr_no + patient_id + subvis + section_id + section_title ~ 
-                            paste("var", str_pad(option_id, 3, side = "left", pad = 0), sep=""),
+                            paste("var", str_pad(field_id, 3, side = "left", pad = 0), sep=""),
                           value.var = c("option_remarks"), 
-                          subset = . (section_id == 1))
+                          subset = . (section_id == 19))
 
+
+chk <- base01_other02 [, .(cnt = .N, 
+                           unqpat = uniqueN(mr_no)), by = .(section_id)]
+
+
+# Read the data
+base01orig_other <- fread("D:/Hospital_data/ProgresSQL/data_chk/base10_other11_orig.csv")
+base01orig_other02 <- base01orig_other [nchar(option_remarks)> 0]
+
+base01orig_other02 <- merge (x = base01orig_other02,
+                         y = section_master,
+                         by = "section_id",
+                         all.x = TRUE)
+chk_orig <- base01orig_other02 [, .(cnt = .N, unqpat = uniqueN(mr_no)), by = .(section_id)]
+
+base01_all <- dplyr::bind_rows(list(base01_other02, base01orig_other02), .id = 'source')
+
+chk_all0 <- base01_all [, .(cnt = .N, unqpat = uniqueN(mr_no)), by = .(section_id)]
+chk_all <- base01_all [, .(cnt = .N, unqpat = uniqueN(mr_no)), by = .(source, section_id)]
+chk_all2 <- dcast (data = chk_all, 
+                   section_id ~ source, 
+                   value.var = c("cnt", "unqpat"))
+chk_all3 <- merge (x = chk_all0, y = chk_all2, by = c("section_id"))
+
+fwrite(chk_all3, "D:/Hospital_data/ProgresSQL/analysis/all_other_data_comparison.csv")
+fwrite(base01_other02, "D:/Hospital_data/ProgresSQL/analysis/all_other_data.csv")
 
 
 section_field_desc <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_desc.csv")
