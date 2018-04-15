f78d96c74e1bba3341a2dbe3a05230732c17f80e
Coursephd <mahajvi@yahoo.co.in>
2018-04-15 06:57:17 +0530

    Add files via upload

--- 02other_data.R
+++ 02other_data.R
@@ -38,11 +38,14 @@ from patient_section_details as a, patient_section_values as b where a.section_d
 #============================================================
 
 library(data.table)
+library(stringi)
+library(stringr)
 
-
+# Read the data
 base01_other <- fread("D:/Hospital_data/ProgresSQL/data_chk/base10_other11.csv")
 base01_other02 <- base01_other [nchar(option_remarks)> 0]
 
+# CRF names
 section_master <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_master.csv")
 section_master <- section_master[, c("section_id", "section_title"), with = FALSE]
 
@@ -51,6 +54,7 @@ base01_other02 <- merge (x = base01_other02,
                        by = "section_id",
                        all.x = TRUE)
 
+# variable names
 section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_options.csv")
 section_field_options <- section_field_options[, c("option_id", "field_id", "display_order", "option_value"), with = FALSE]
 
@@ -58,7 +62,25 @@ base01_other02 <- merge (x = base01_other02,
                          y = section_field_options,
                          by = c("option_id", "field_id"),
                          all.x = TRUE)
+# Keep Unique records
+base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
+
+# Sort the data by patient and visits
 base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, option_id, field_id)]
+
+# Do the clean-up where the variable name is not present
+base01_other03 <- base01_other02 [ nchar(option_value) > 0]
+base01_other04 <- base01_other02 [ option_id < 0]
+
+# Create a counter variable for transpose
+base01_other030 <- base01_other03 [, subvis := 1:.N, by = .(mr_no, patient_id, section_id, option_id, field_id)]
+
+# Transpose the data as per CRF pages
+base01_other030t <- dcast(data = base01_other030,
+                          mr_no + patient_id + subvis + section_id + section_title ~ 
+                            paste("var", str_pad(option_id, 3, side = "left", pad = 0), sep=""),
+                          value.var = c("option_remarks"), 
+                          subset = . (section_id == 1))
 
 
 
@@ -69,6 +91,6 @@ section_field_options02 <- section_field_options[, c("option_id", "field_id", "o
 
 section_field_options03 <- section_field_options02 [ field_id == 67]
   
-sub <- base01_other02 [mr_no == "MR040505"]
+sub <- base01_other030 [patient_id == "IP002102"]
 
 counts <- base01_other02 [, cnt :=.N, by =.(mr_no, patient_id, section_id, field_id, option_id)]
