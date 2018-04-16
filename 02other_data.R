2de1d0678c432ac232d8d527080b61e854d900d3
Coursephd <mahajvi@yahoo.co.in>
2018-04-16 21:56:57 +0530

    Add files via upload
    One of the first correct attempts to get variables and values aligned
    
    Need to sort out issues in base01_other022 data.table

--- 02other_data.R
+++ 02other_data.R
@@ -73,26 +73,39 @@ base01_other02 <- merge (x = base01_other02,
                        all.x = TRUE)
 
 # variable names
-#section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_options.csv")
-section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_desc.csv")
-#section_field_options <- section_field_options[, c("option_id", "field_id", "display_order", "option_value"), with = FALSE]
+section_field_options <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_options.csv")
 
-base01_other02 <- merge (x = base01_other02,
-                         y = section_field_options,
-                         by = c("section_id", "field_id"), # by = c("option_id", "field_id")
+base01_other022 <- merge (x = base01_other02 [ option_id >= 0],
+                         y = section_field_options ,
+                         by = c("option_id", "field_id"), #by = c("section_id", "field_id"), 
                          all.x = TRUE)
 
 # Keep Unique records
-#base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
-base01_other02 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id",  "field_id", "option_remarks", "section_title", "display_order", "field_name"), with =FALSE] )
+#base01_other022 <- unique ( base01_other02 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
+base01_other022 <- unique ( base01_other022 [, c("mr_no", "patient_id", "section_id",  "field_id", "option_remarks", "section_title", "display_order", "option_value"), with =FALSE] )
 
 # Sort the data by patient and visits
-#base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, option_id, field_id)]
-base01_other02 <- base01_other02 [ order(mr_no, patient_id, section_id, field_id)]
+base01_other022 <- base01_other022 [ order(mr_no, patient_id, section_id, field_id, display_order)]
 
-# Do the clean-up where the variable name is not present
-base01_other03 <- base01_other02 [ nchar(field_name) > 0]
-base01_other04 <- base01_other02 [ field_id < 0]
+section_field_desc <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_desc.csv")
+section_field_desc <- section_field_desc[, c("section_id", "field_id", "display_order", "field_name", "no_of_lines"), with = FALSE]
+
+base01_other044 <- merge (x = base01_other02 [ option_id < 0],
+                         y = section_field_desc,
+                         by = c("section_id", "field_id"), 
+                         all.x = TRUE)
+base01_other044 <- unique ( base01_other044 [, c("mr_no", "patient_id", "section_id", "option_id", "field_id", "option_remarks", "section_title", "display_order", "field_name", "no_of_lines"), with =FALSE] )
+
+# Sort the data by patient and visits
+base01_other044 <- base01_other044 [ order(mr_no, patient_id, section_id, field_id, display_order)]
+
+
+########################################################
+# Need to consolidate variable names and combine 
+# base01_other044
+# base01_other022
+# Create a counter variable for transposing
+########################################################
 
 # Create a counter variable for transpose
 base01_other030 <- base01_other03 [, subvis := 1:.N, by = .(mr_no, patient_id, section_id, field_id, field_name)]
@@ -102,7 +115,7 @@ base01_other030t <- dcast(data = base01_other030,
                           mr_no + patient_id + subvis + section_id + section_title ~ 
                             paste("var", str_pad(field_id, 3, side = "left", pad = 0), sep=""),
                           value.var = c("option_remarks"), 
-                          subset = . (section_id == 19))
+                          subset = . (section_id == 85))
 
 
 chk <- base01_other02 [, .(cnt = .N, 
@@ -117,6 +130,12 @@ base01orig_other02 <- merge (x = base01orig_other02,
                          y = section_master,
                          by = "section_id",
                          all.x = TRUE)
+
+base01orig_other02 <- merge (x = base01orig_other02,
+                         y = section_field_options,
+                         by = c("section_id", "field_id"), # by = c("option_id", "field_id")
+                         all.x = TRUE)
+
 chk_orig <- base01orig_other02 [, .(cnt = .N, unqpat = uniqueN(mr_no)), by = .(section_id)]
 
 base01_all <- dplyr::bind_rows(list(base01_other02, base01orig_other02), .id = 'source')
@@ -130,7 +149,17 @@ chk_all3 <- merge (x = chk_all0, y = chk_all2, by = c("section_id"))
 
 fwrite(chk_all3, "D:/Hospital_data/ProgresSQL/analysis/all_other_data_comparison.csv")
 fwrite(base01_other02, "D:/Hospital_data/ProgresSQL/analysis/all_other_data.csv")
+fwrite(section_master, "D:/Hospital_data/ProgresSQL/analysis/section_master.csv")
 
+
+fwrite(base01_other02, "D:/Hospital_data/ProgresSQL/analysis/all_other_data_Using_section_field_options.csv")
+
+base01_all01 <- unique ( base01_all [, c("mr_no", "section_id", "source"), with =FALSE])
+base01_all01 <- base01_all01 [, val:=1]
+base01_all01t <- dcast(data =base01_all01,
+                       mr_no + section_id ~ source, 
+                       value.var = "val", 
+                       fill =0)
 
 section_field_desc <- fread("D:/Hospital_data/ProgresSQL/data_chk/section_field_desc.csv")
 patient_consultation_field_values <- fread("D:/Hospital_data/ProgresSQL/data_chk/patient_consultation_field_values.csv")
