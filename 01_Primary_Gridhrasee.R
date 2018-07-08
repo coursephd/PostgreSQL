78427ebf64a27ad721e86d3735fb6659e8041a78
coursephd <mahajvi@yahoo.co.in>
2018-07-08 09:41:27 +0530

    Add files via upload

---
+++ 01_Primary_Gridhrasee.R
@@ -0,0 +1,39 @@
+library(Hmisc)
+library(data.table)
+library(stringi)
+library(stringr)
+library(sqldf)
+
+all_met_rmsd <- readRDS("D:/Hospital_data/ProgresSQL/analysis/01adsl_met_rmsd.rds")
+
+all_met_rmsd <- all_met_rmsd [, `:=` (vismon = round( cdur/30.4375, digits = 0))]
+all_met_rmsd <- all_met_rmsd [Code !=""]
+
+madhumeha <- unique( all_met_rmsd [ Code %in% c("V2.23") & diag_type == "P", c("mr_no"), ])
+all_sub <- all_met_rmsd [ mr_no %in% madhumeha$mr_no]
+
+# Analysis for the disease duration
+dis_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
+                                daystt = min(studyday ), dayend = max( studyday),
+                                disdur = as.numeric(max(newdt0) - min(newdt0) + 1)), 
+                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, Code, description)] )
+
+dis_data <- dis_data [, cat := "Disease"]
+
+# Analysis for the disease duration
+med_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
+                                daystt = min(studyday ), dayend = max( studyday),
+                                disdur = as.numeric(max(newdt0) - min(newdt0) + 1)), 
+                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, medicine_name)] )
+
+setnames(med_data, "medicine_name", "description")
+med_data <- med_data [, cat := "Medicine"]
+
+all_data <- rbind(dis_data, med_data, fill =TRUE)
+
+fwrite(all_data, 
+       "D:/Hospital_data/ProgresSQL/analysis/Primary_gridhrasee.csv")
+
+#####################################################################################
+# End of program
+#####################################################################################
