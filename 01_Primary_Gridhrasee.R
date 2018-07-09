5160a75820c023968611e0fb2ca5d144404570f9
coursephd <mahajvi@yahoo.co.in>
2018-07-09 07:17:55 +0530

    Add files via upload

--- 01_Primary_Gridhrasee.R
+++ 01_Primary_Gridhrasee.R
@@ -21,12 +21,15 @@ dis_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
 dis_data <- dis_data [, cat := "Disease"]
 
 # Analysis for the disease duration
+#                             by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, medicine_name)] )
+
 med_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
                                 daystt = min(studyday ), dayend = max( studyday),
                                 disdur = as.numeric(max(newdt0) - min(newdt0) + 1)), 
-                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, medicine_name)] )
+                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, Coded_med, Type_med)] )
 
-setnames(med_data, "medicine_name", "description")
+setnames(med_data, "Coded_med", "description")
+setnames(med_data, "Type_med", "Code")
 med_data <- med_data [, cat := "Medicine"]
 
 all_data <- rbind(dis_data, med_data, fill =TRUE)
