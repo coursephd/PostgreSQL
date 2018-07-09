00e57e3b1f7870c3b916bf153c00b3131e624d4e
coursephd <mahajvi@yahoo.co.in>
2018-07-09 07:05:38 +0530

    Add files via upload

--- 01_Primary_madhumeha.R
+++ 01_Primary_madhumeha.R
@@ -12,7 +12,6 @@ library(ggplot2)
 
 all_met_rmsd <- readRDS("D:/Hospital_data/ProgresSQL/analysis/01adsl_met_rmsd.rds")
 
-all_met_rmsd <- all_met_rmsd [, `:=` (baseage = min(age)), by =.(mr_no)]
 all_met_rmsd <- all_met_rmsd [, `:=` (vismon = round( cdur/30.4375, digits = 0))]
 all_met_rmsd <- all_met_rmsd [Code !=""]
 
@@ -28,12 +27,15 @@ dis_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
 dis_data <- dis_data [, cat := "Disease"]
 
 # Analysis for the disease duration
+#                             by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, medicine_name)] )
+
 med_data <- unique(all_sub [, .(disstt = min(newdt0 ), disend = max( newdt0),
                                 daystt = min(studyday ), dayend = max( studyday),
                                 disdur = as.numeric(max(newdt0) - min(newdt0) + 1)), 
-                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, medicine_name)] )
+                            by = .(distype, mr_no, patient_gender, baseage, all_vis, all_ip, all_op, cdur, cstdt,cendt, Coded_med, Type_med)] )
 
-setnames(med_data, "medicine_name", "description")
+setnames(med_data, "Coded_med", "description")
+setnames(med_data, "Type_med", "Code")
 med_data <- med_data [, cat := "Medicine"]
 
 all_data <- rbind(dis_data, med_data, fill =TRUE)
