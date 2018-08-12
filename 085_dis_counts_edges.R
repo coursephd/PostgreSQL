9e33e5af964ed1462aef648125774ffab1f42cbd
coursephd <mahajvi@yahoo.co.in>
2018-08-12 14:29:27 +0530

    Add files via upload

--- 085_dis_counts_edges.R
+++ 085_dis_counts_edges.R
@@ -67,9 +67,9 @@ path01 <- merge (x = chk02all,
                  by = c("cat", "refcode", "refdesc", "Code", "description"),
                  all = TRUE)
 path01 <- path01 [, `:=` (cat = "DiseaseMedicine", Code = Code02),] 
+path01 <- path01 [, c("TabCode", "TabMed") := tstrsplit(Code, "->", fixed = TRUE), ]
 
 chk03all <- rbind(path01, dismed, fill = TRUE)
-chk03all <- chk03all [, c("TabCode", "TabMed") := tstrsplit(Code, "->", fixed = TRUE), ]
 
 fwrite(chk03all, 
        "D:/Hospital_data/ProgresSQL/analysis/085_dis_count_edges_2nd.csv")
