8a69cf4ba06bee6230f977ae2a21b81415077d39
coursephd <mahajvi@yahoo.co.in>
2018-09-13 09:51:04 +0530

    Add files via upload

--- 086_dis_patterns_combinations.R
+++ 086_dis_patterns_combinations.R
@@ -1,56 +1,3 @@
-# attempt based on the binary measures papers:
-
-library(tidyverse)
-library(tidytext)
-library(stringr)
-library(data.table)
-library(stringdist)
-library(reshape2)
-
-data <-fread ("C:\\Users\\mahajvi1\\Desktop\\dis01.csv", sep =",")
-
-unq01comb <- unique( data [, c("mr_no", "refcode", "refdesc", "alldis", "ndis", "totrow"), ])
-unq01comb <- unq01comb [, x := 1, ]
-unq01comb <- unq01comb [, alldis := str_replace_all(alldis, " ", ""), ]
-
-setnames(unq01comb, "alldis", "combdis")
-
-# create a copy
-unq02comb <- copy(unq01comb)
-setnames(unq02comb, "mr_no", "mr_no2")
-setnames(unq02comb, "combdis", "combdis2")
-
-# Merge the datasets on x to get all the combinations
-
-unq03comb <- merge(x = unq01comb, 
-                   y = unq02comb [, -c("refcode", "refdesc", "ndis", "totrow"), ], 
-                   by = c("x"), 
-                   allow.cartesian = TRUE)
-
-########################################################
-# Using str_count function to count the common diseases
-# Create tempdis and tempdis2
-#
-# Consider mr_no as the reference patient
-# tempdis: should be lookup
-# a: common in both the strings
-# b: only present in reference patient (mr_no)
-# c: only present in other patient (mr_no2)
-# d: complete absence -- not sure how to calculate this
-########################################################
-
-unq03comb <- unq03comb [, `:=` (tempdis = str_replace_all(combdis, ",", "|"), 
-                                tempdis2 = str_replace_all(combdis2, ",", "|"),
-                                cntdis = str_count(combdis, ",") + 1, 
-                                cntdis2 = str_count(combdis2, ",") + 1), ]
-
-unq03comb <- unq03comb[, `:=` (a = str_count(combdis2, tempdis)),]
-
-unq03comb <- unq03comb [, `:=` (b = cntdis - a,
-                               c = cntdis2 - a),  ]
-
-distmsr <- unq03comb[, a01jac := (a / (a + b + c)),]
-
 ####################################################################
 #
 ####################################################################
@@ -68,7 +15,9 @@ all_met_rmsd <- readRDS ("D:/Hospital_data/ProgresSQL/analysis/all_met_rmsd02.rd
 a2 <- all_met_rmsd [!Code %in% c("", " ", "A2.0") & refcode == "A2.0"]
 
 dis <- unique(a2[!Code %in% c("", " ", "A2.0") & refcode == "A2.0", c("mr_no", "studyday", "refday", "Code", "description", "refcode", "refdesc")])
-dis <- dis [, refday2 := ifelse(refday >=1, "After", "Before"), ]
+dis <- dis [, `:=` (refday2 = ifelse(refday >=1, "After", "Before"), 
+                    Code = str_replace_all (Code, " ", ""),
+                    description = str_replace_all(description, " ", "")),]
 dis <- dis [ order(mr_no, studyday, Code, refcode, refdesc)]
 dis <- dis [, `:=` (alldis = uniqueN(Code), 
                     nrow = seq_len(.N),
@@ -77,7 +26,7 @@ dis <- dis [, `:=` (alldis = uniqueN(Code),
 dis <- dis [, `:=` (alldisbfraftr = uniqueN(Code), 
                     nrowbfraftr = seq_len(.N) ), by = .(mr_no, refcode, refdesc, refday2)]
 
-dis02 <- dis [, .(combdis = paste(unique(description), collapse = ",", sep = " " )), 
+dis02 <- dis [, .(combdis = paste(unique(Code), collapse = ",", sep = " " )), 
               by = .(mr_no, refcode, refdesc, refday2, alldis, totrow)]
 
 unq01comb <- unique( dis02 [, c("mr_no", "refcode", "refdesc", "alldis", "refday2",
@@ -93,10 +42,49 @@ unq01comb <- unq01comb [, combdis := str_replace_all(combdis, ",", "|"), ]
 
 # Merge the datasets on x to get all the combinations
 
-unq03comb <- merge(x = unq01comb, # [ mr_no == "MR000002"], 
+unq03comb <- merge(x = unq01comb, 
                    y = unq02comb [, -c("refcode", "refdesc", "totrow", "alldis"), ], 
                    by = c("x", "refday2"), 
                    allow.cartesian = TRUE)
+
+########################################################
+# Using str_count function to count the common diseases
+# Create tempdis and tempdis2
+#
+# Consider mr_no as the reference patient
+# tempdis: should be lookup
+# a: common in both the strings
+# b: only present in reference patient (mr_no)
+# c: only present in other patient (mr_no2)
+# d: complete absence -- not sure how to calculate this
+########################################################
+
+unq03comb <- unq03comb [, `:=` (tempdis = str_replace_all(combdis, ",", "|"), 
+                                tempdis2 = str_replace_all(combdis2, ",", "|")),]
+
+unq03comb <- unq03comb [, `:=` (cntdis = str_count(tempdis, "\\|") + 1, 
+                                cntdis2 = str_count(tempdis2, "\\|") + 1), ]
+
+unq03comb <- unq03comb[, `:=` (a = str_count(combdis2, tempdis)),]
+
+unq03comb <- unq03comb [, `:=` (b = cntdis - a,
+                                c = cntdis2 - a),  ]
+
+unq03comb <- unq03comb[, a01jac := (a / (a + b + c)),]
+
+cnt <- unq03comb [, .(cnt = uniqueN( paste(mr_no, mr_no2))), by = .(a01jac)]
+
+chk022 <- melt(data = chk02,
+               id.vars = 1:10,
+               measure.vars = 11:20,
+               variable.name = "distmethod")
+
+fwrite(unq03comb, 
+       "D:/Hospital_data/ProgresSQL/analysis/086_dis_patterns_combinations_A2.0.csv")
+
+####################################################################
+# End of program
+####################################################################
 
 ########################################################
 # Using str_count function to count the common diseases
