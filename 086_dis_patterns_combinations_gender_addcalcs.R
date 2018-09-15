bc4263c4edb22da9391985b1278b991cfe24df56
coursephd <mahajvi@yahoo.co.in>
2018-09-15 22:17:08 +0530

    Add files via upload

---
+++ 086_dis_patterns_combinations_gender_addcalcs.R
@@ -0,0 +1,101 @@
+####################################################################
+# 086_dis_patterns_combinations_gender_addcalcs.R
+####################################################################
+
+library(tidyverse)
+library(tidytext)
+library(stringr)
+library(stringi)
+library(data.table)
+library(stringdist)
+
+# https://stackoverflow.com/questions/43706729/expand-dates-in-data-table
+
+all_met_rmsd <- readRDS ("D:/Hospital_data/ProgresSQL/analysis/all_met_rmsd02.rds")
+a2 <- all_met_rmsd [!Code %in% c("", " ") & refcode == "M2.0"]
+
+a2med <- a2 [, description := paste(Type_med, Coded_med),]
+a2med <- a2med [ order(description)]
+a2med <- a2med [, Code := paste("M", str_pad(.N, width =4, pad="0"), sep =""), by = .(description) ]
+
+a2all <- rbind(a2 [, c("mr_no", "studyday", "refday", "Code", "description", "refcode", "refdesc", "patient_gender")], 
+               a2med [, c("mr_no", "studyday", "refday", "Code", "description", "refcode", "refdesc", "patient_gender")] )
+
+# Change a2 to a2all
+dis <- unique(a2[, c("mr_no", "studyday", "refday", "Code", "description", "refcode", "refdesc", "patient_gender")])
+dis <- dis [, `:=` (refday2 = ifelse(refday >=1, paste("After",patient_gender, sep =""),  paste("Before", patient_gender, sep="")), 
+                    Code = str_replace_all (Code, " ", ""),
+                    description = str_replace_all(description, " ", "")),]
+dis <- dis [ order(mr_no, studyday, Code, refcode, refdesc)]
+dis <- dis [, `:=` (alldis = uniqueN(Code), 
+                    nrow = seq_len(.N),
+                    nrowend = seq_len(.N) + 4,
+                    totrow = .N), by = .(mr_no, refcode, refdesc)]
+dis <- dis [, `:=` (alldisbfraftr = uniqueN(Code), 
+                    nrowbfraftr = seq_len(.N) ), by = .(mr_no, refcode, refdesc, refday2)]
+
+dis02 <- dis [, .(combdis = paste(unique(Code), collapse = ",", sep = " " )), 
+              by = .(mr_no, refcode, refdesc, refday2, alldis, totrow)]
+
+unq01comb <- unique( dis02 [, c("mr_no", "refcode", "refdesc", "alldis", "refday2",
+                                "totrow",  "combdis"), ])
+unq01comb <- unq01comb [, x := 1, ]
+
+# create a copy
+unq02comb <- copy(unq01comb)
+setnames(unq02comb, "mr_no", "mr_no2")
+setnames(unq02comb, "combdis", "combdis2")
+
+unq01comb <- unq01comb [, combdis := str_replace_all(combdis, ",", "|"), ]
+
+# Merge the datasets on x to get all the combinations
+
+unq03comb <- merge(x = unq01comb, 
+                   y = unq02comb [, -c("refcode", "refdesc", "totrow", "alldis"), ], 
+                   by = c("x", "refday2"), 
+                   allow.cartesian = TRUE)
+
+########################################################
+# Using str_count function to count the common diseases
+# Create tempdis and tempdis2
+#
+# Consider mr_no as the reference patient
+# tempdis: should be lookup
+# a: common in both the strings
+# b: only present in reference patient (mr_no)
+# c: only present in other patient (mr_no2)
+# d: complete absence -- not sure how to calculate this
+########################################################
+
+unq03comb <- unq03comb [, `:=` (tempdis = str_replace_all(combdis, ",", "|"), 
+                                tempdis2 = str_replace_all(combdis2, ",", "|")),]
+
+unq03comb <- unq03comb [, `:=` (cntdis = str_count(tempdis, "\\|") + 1, 
+                                cntdis2 = str_count(tempdis2, "\\|") + 1), ]
+
+unq03comb <- unq03comb[, `:=` (a = str_count(combdis2, tempdis)),]
+
+unq03comb <- unq03comb [, `:=` (b = cntdis - a,
+                                c = cntdis2 - a),  ]
+
+unq03comb <- unq03comb[, a01jac := (a / (a + b + c)),]
+unq03comb <- unq03comb [ mr_no != mr_no2]
+
+
+maxscr <- unq03comb[, .(maxscr = max(a01jac) ), by = .(mr_no, totrow, alldis, refday2, combdis)]
+maxscr_t <- dcast (data = maxscr, 
+                   mr_no + totrow + alldis ~ refday2, 
+                   value.var = c("maxscr", "combdis"))
+
+maxscr02 <- maxscr [, .(scr = uniqueN(mr_no)), by = .(maxscr, refday2)]
+maxscr02_t <- dcast(data = maxscr02, 
+                    maxscr ~ refday2,
+                    value.var = c("scr"))
+
+
+maxscr03 <- maxscr [, .(scr = uniqueN(mr_no)), by = .(maxscr, combdis, refday2)]
+maxscr03_t <- dcast(data = maxscr03, 
+                    combdis + maxscr ~ refday2,
+                    value.var = c("scr"))
+
+dismaxscr02_t <- maxscr02_t
