1a4cd5a588e00e6a119b188b9d21d7649b12abb3
coursephd <mahajvi@yahoo.co.in>
2019-01-05 15:04:59 +0530

    Add files via upload

--- 088_spmf_SPADE.R
+++ 088_spmf_SPADE.R
@@ -1,16 +1,7 @@
-# Paper shared by Kaustav
-# https://arxiv.org/pdf/1307.1411.pdf
-
 library(data.table)
 library(tidyverse)
 
-all_met_rmsd02 <- readRDS("C:/Users/mahajvi1/Downloads/all_met_rmsd02.rds")
-
-amavat <- all_met_rmsd02 [ refcode == "A2.0"]
-amavatpst <- amavat [ refday >= 1 & Code == "A2.0"]
-
-
-all_met_rmsd02 <- readRDS("C:/Users/mahajvi1/Downloads/all_met_rmsd02.rds")
+all_met_rmsd02 <- readRDS("D:/Hospital_data/ProgresSQL/analysis/all_met_rmsd02.rds")
 all_met_rmsd02 <- all_met_rmsd02 [, Code := ifelse (Code == " " | Code == "", "** Not yet coded", Code),]
 all_met_rmsd02 <- all_met_rmsd02 [, description:= ifelse (description == "" | description ==" ", "** Not yet coded", description),]
 all_met_rmsd02 <- all_met_rmsd02 [, Code02 := paste(Code, ":", description, sep =""), ]
@@ -38,7 +29,7 @@ all_met_rmsd06 <- all_met_rmsd06 [, combdis02 := paste(combdis02, " -1 -2", sep 
 
 fwrite(x = all_met_rmsd06 [, c("combdis02"),], 
        col.names = FALSE,
-       file = "C:/Users/mahajvi1/Downloads/spmf_SPADE.txt")
+       file = "D:/Hospital_data/ProgresSQL/analysis_spmf/spmf_SPADE_M2.0.txt")
 
 disnum <- unique( all_met_rmsd02 [, c("Code02", "Code", "comdisn"),])
 
@@ -48,12 +39,25 @@ disnum <- unique( all_met_rmsd02 [, c("Code02", "Code", "comdisn"),])
 # FPGrowth_association_rules
 
 all_met_rmsd06_arff <- all_met_rmsd05 [, .(combdis02 = paste(combdis, collapse = " ", sep = " " )), 
-                                  by = .(mr_no, refcode, refdesc, baseage)]
+                                       by = .(mr_no, refcode, refdesc, baseage)]
 
 fwrite(x = all_met_rmsd06_arff [, c("combdis02"),], 
        col.names = FALSE,
-       file = "C:/Users/mahajvi1/Downloads/spmf_ARFF.txt")
-#=====================================================================================================================
+       file = "D:/Hospital_data/ProgresSQL/analysis_spmf/spmf_ARFF_M2.0.txt")
+
+
+
+out <- fread("D:/Hospital_data/ProgresSQL/analysis_spmf/spmf_ARFFoutput_TopKRules_M2.0.txt", 
+             sep ="!",
+             header = FALSE)
+
+out2 <- out [, c("var01", "var02", "var03") := tstrsplit(V1, "#"),]
+out3 <- out2 [, c("var021", "var022") := tstrsplit(var01, "==>"),]
+out4 <- out3 [, `:=` (cntvar021 = str_count( trimws(var021), " ") + 1,
+                      cntvar022 = str_count( trimws(var022), " ") + 1),]
+
+out5 <- out4 [, paste0("type", 1:max(out3$cntvar021)) := tstrsplit(var021, " "),]
+
 
 
 library(openxlsx)
