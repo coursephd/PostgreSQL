34dc4714acfc7789bffbae47d2ac520b4ef204f4
coursephd <mahajvi@yahoo.co.in>
2019-05-22 13:34:40 +0530

    Update 102_episodic_view.R

--- 102_episodic_view.R
+++ 102_episodic_view.R
@@ -2,7 +2,8 @@ library(data.table)
 library(tidyverse)
 library(sqldf)
 
-all_met_rmsd02 <- readRDS("D:/Hospital_data/ProgresSQL/analysis/01adsl_met_rmsd.rds")
+all_met_rmsd02 <- fread("C://Users//mahajvi1//Downloads//01adsl_met_rmsd.csv")
+#all_met_rmsd02 <- readRDS("D:/Hospital_data/ProgresSQL/analysis/01adsl_met_rmsd.rds")
 all_met_rmsd02 <- all_met_rmsd02 [, Code := ifelse (Code == " " | Code == "", "** Not yet coded", Code),]
 all_met_rmsd02 <- all_met_rmsd02 [, description:= ifelse (description == "" | description ==" ", "** Not yet coded", description),]
 all_met_rmsd02 <- all_met_rmsd02 [, Code02 := paste(Code, ":", description, sep =""), ]
@@ -14,74 +15,42 @@ all_met_rmsd03 <- all_met_rmsd03 [, distime := 1:.N, by = .(mr_no, Code02)]
 all_met_rmsd03 <- all_met_rmsd03 [, diff := studyday - shift(studyday), by = .(mr_no, Code, description)]
 all_met_rmsd03 <- all_met_rmsd03 [, diff := ifelse (distime ==1, 1, diff), ]
 
-
 # For merge
-pat <- unique( all_met_rmsd02 [, c("mr_no", "Metabolic", "RMSD", "patient_gender",
-                                   "baseage", "all_vis", "cdur")])
+pat <- unique( all_met_rmsd02 [, c("mr_no", "Metabolic", "RMSD", "patient_gender", "baseage",
+                                    "all_vis", "cdur")])
 
-#################################################################################
 # If all_vis = 1 then patient level only 1 visit
-# Category and explanation
-#
-# 100 = Drop out with no data                       | 100 Drop outs
-# 200 = Drop out with limited data                  |
-#
-# 300 = Drop out with approx 1 month data           | 200 Drop outs limited data
-# 400 = Limited visits with 1 to 6 month data?      |
-#
-# 500 = Large visits with  1 to 6 month data?       | 300 Limited data
-# 600 = More than 6 months data                     | 400 Large data
-#
-#################################################################################
-
-#####################
-# 6 category program
-#####################
 
 all_met_rmsd04 <- all_met_rmsd03 %>%
   mutate(
-    patdis =
+    patdis = 
       case_when( all_vis == 1 ~ 100,
                  all_vis > 1 & all_vis <= 5 ~ 200,
                  all_vis > 5 & cdur <= 30 ~ 300,
                  all_vis > 5 & all_vis <= 10 & cdur > 30 & cdur <= 180 ~ 400,
-                 all_vis > 10 & cdur > 30 & cdur <= 180 ~ 500,
+                 all_vis > 10 & cdur > 30 & cdur < 180 ~ 500,
                  cdur > 180 ~ 600
-      )
-  )
-
-
-#####################
-# 4 category program
-#####################
-
-all_met_rmsd04 <- all_met_rmsd03 %>%
-  mutate(
-    patdis =
-      case_when( all_vis == 1 ~ 100,
-                 all_vis > 1 & all_vis <= 5 ~ 100,
-                 all_vis > 5 & cdur <= 30 ~ 200,
-                 all_vis > 5 & all_vis <= 10 & cdur > 30 & cdur <= 180 ~ 200,
-                 all_vis > 10 & cdur > 30 & cdur <= 180 ~ 300,
-                 cdur > 180 ~ 400
+                 
       )
   )
 
 all_met_rmsd04 <- as.data.table(all_met_rmsd04)
+
 chk <- all_met_rmsd04 [, .(cnt = uniqueN(mr_no)), by = .(patdis)]
+
 
 ###############################################################################
 # Create a variable to identify episodes of a disease
 # if a disease is re-appearing after 30 days then consider that as
 # a new episode, this duration should be specific to each disease in reality
-# Use the variable to eps01 for cumulative addition and get number of
+# Use the variable to eps01 for cumulative addition and get number of 
 # episodes for each disease for each patient
 #
 # If a disease is non-episodic then use 9999 as the duration
 # This calculation should help in understanding the disease specific
 # pseudo outcome and amount of data collected
-#
-# Use this variable along with the overall classification of a patient to
+# 
+# Use this variable along with the overall classification of a patient to 
 # create a medical story
 ###############################################################################
 
@@ -91,10 +60,20 @@ all_met_rmsd04 <- all_met_rmsd04 [, eps011 := cumsum(eps01) + 1, by = .(mr_no, C
 # Create episodic visit numbering
 all_met_rmsd04 <- all_met_rmsd04 [, eps_vis := sequence(.N), by = .(mr_no, Code02, Code, description, eps011)]
 
-# Calculate duration of invidual disease episode
-all_met_rmsd04 <- all_met_rmsd04 [, `:=` (epsdur = max(studyday) - min (studyday) + 1), by = .(mr_no, Code02, Code, description, eps011)]
+# Calculate the difference between 2 episodes
+all_met_rmsd04 <- all_met_rmsd04 [, diffeps := studyday - shift(studyday), by = .(mr_no, Code02, Code, description)]
+all_met_rmsd04 <- all_met_rmsd04 [, diffeps := ifelse (eps011 ==1 & eps_vis ==1, 0, diffeps), ]
+all_met_rmsd04_11 <- unique( all_met_rmsd04 [ eps_vis ==1, c("mr_no", "Code02", "Code", "description", "eps011", "diffeps"), ] )
 
-epsd01 <- all_met_rmsd04 [, .(epsdur = max(studyday) - min (studyday) + 1), by = .(mr_no, Code02, Code, description, eps011)]
+all_met_rmsd04 <- merge (x = all_met_rmsd04 [, -c("diffeps"),],
+                         y = all_met_rmsd04_11,
+                         by = c("mr_no", "Code02", "Code", "description", "eps011"),
+                         all.x = TRUE)
+
+# Calculate duration of invidual disease episode
+all_met_rmsd04 <- all_met_rmsd04 [, `:=` (epsdur = max(studyday) - min (studyday) + 1), by = .(mr_no, Code02, Code, description, eps011, diffeps)]
+
+epsd01 <- all_met_rmsd04 [, .(epsdur = max(studyday) - min (studyday) + 1), by = .(mr_no, Code02, Code, description, eps011, diffeps)]
 
 # calculate the median after removing events with only 1 day
 all_met_rmsd05 <- epsd01 [epsdur > 1]
@@ -121,6 +100,13 @@ t01_exl01 <- epsd01 [epsdur > 1, .(n = uniqueN(mr_no),
                                    median = median (epsdur, na.rm = FALSE),
                                    min = min (epsdur, na.rm = FALSE),
                                    max = max (epsdur, na.rm = FALSE)), by = .(Code02, eps011)]
+
+t01_diffeps <- epsd01 [diffeps > 0, .(n = uniqueN(mr_no),
+                                   mean = mean(diffeps, na.rm = FALSE),
+                                   sd = sd(diffeps, na.rm = FALSE),
+                                   median = median (diffeps, na.rm = FALSE),
+                                   min = min (diffeps, na.rm = FALSE),
+                                   max = max (diffeps, na.rm = FALSE)), by = .(Code02, eps011)]
 
 
 t01resp <- all_met_rmsd05 [, .(n = uniqueN(mr_no),
