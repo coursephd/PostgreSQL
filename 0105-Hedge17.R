f05ba6a77da30bef6b6760f54cf95ae2beba48a8
coursephd <mahajvi@yahoo.co.in>
2020-08-07 21:57:25 +0530

    Add files via upload

--- 0105-Hedge17.R
+++ 0105-Hedge17.R
@@ -65,8 +65,12 @@ bnkexp02 <- bnkexp02 [, strk := ifelse(OPENFUT < 10000, signif(OPENFUT, 2), sign
 
 # Pick up the strike on day 4 or 5 and add 1400 or 1500, 1600, 1700
 
-bnkexp03 <- unique( na.omit( bnkexp02 [ nrow %in% c(4, 5), c("SYMBOL", "EXPIRY_DT", "strk", "nrow"), ]) )
-bnkexp03 <- bnkexp03 [, `:=`(strk1 = strk + 1300, strk2 = strk + 1400, strk3 = strk + 1500, strk4 = strk + 1600, strk5 = strk + 1700), ]
+bnkexp03 <- unique( na.omit( bnkexp02 [ nrow %in% c(1, 2, 3, 4, 5), c("SYMBOL", "EXPIRY_DT", "strk", "nrow"), ]) )
+bnkexp03 <- bnkexp03 [, `:=`(strk1300 = strk + 1300, 
+                             strk1400 = strk + 1400, 
+                             strk1500 = strk + 1500, 
+                             strk1600 = strk + 1600, 
+                             strk1700 = strk + 1700), ]
 bnkexp03_t <- melt(data = bnkexp03, 
                    id.vars = c("SYMBOL", "EXPIRY_DT", "strk", "nrow")  )
 setnames (bnkexp03_t, "nrow", "entryday")
@@ -86,6 +90,34 @@ bnkexp100 <- bnkexp05 [ nrow == 1]
 bnkexp100 <- bnkexp100 [, ntrades := .N,]
 bnkexp100 <- bnkexp100 [, pftloss := ifelse(LOW < 1, "Profit", "Loss"),]
 bnkexp101 <- bnkexp100 [, .(cnt = .N), by = .(pftloss, ntrades)]
+
+# Pick up Nifty on any of the first 5 days in the 100s range:
+
+nifty001 <- opt03 [ SYMBOL == "NIFTY" & toupper(mrgdt) == toupper( substr(TIMESTAMP, 4, length(TIMESTAMP) ) ) ]
+nifty001 <- nifty001 [, `:=` (day = format(trday, "%d"),
+                              minval = pmin(OPEN, CLOSE, LOW, HIGH), 
+                              maxval = pmax(OPEN, CLOSE, LOW, HIGH) ), ]
+nifty001 <- nifty001 [, mnthexp := max(nexpday), by = .(mrgdt) ]
+nifty002 <- nifty001 [ (minval >= 70 & minval <= 130) & 
+                         mnthexp == nexpday  ]
+
+nifty003 <- nifty002 [, c("SYMBOL", "trday", "STRIKE_PR", "OPEN", "CLOSE", "LOW", "HIGH", "mrgdt")]
+nifty003 <- nifty003 [, entrydt := trday,]
+setnames (nifty003, "OPEN", "OPENentry")
+setnames (nifty003, "CLOSE", "CLOSEentry")
+setnames (nifty003, "LOW", "LOWentry")
+setnames (nifty003, "HIGH", "HIGHentry")
+
+nifty004 <- merge(x = nifty003 [, -c("trday"), ],
+                  y = nifty001,
+                  by = c("SYMBOL", "STRIKE_PR", "mrgdt"),
+                  allow.cartesian = TRUE)
+nifty005 <- nifty004 [ entrydt <= trday & mnthexp == nexpday ]
+
+# Calculate the number of trades by creating a group
+# SYMBOL, STRIKE_PR, mrgdt, entrydt, trday, nexpday, EXPIRY_DT
+nifty005 <- nifty005 [, tradenum := 1:.N, by = .(SYMBOL, STRIKE_PR, mrgdt, entrydt, nexpday, EXPIRY_DT)]
+nifty005 <- nifty005 [, tradetot := .N, by = .(mrgdt)]
 
 # 30th April 2020 example: Nifty and Bank Nifty went mad and increased over 2000 points in a week
 
