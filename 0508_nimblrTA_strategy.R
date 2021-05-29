3d71a961c3301b14026fcada485814af51fac308
coursephd <mahajvi@yahoo.co.in>
2021-05-29 19:45:25 +0530

    Add files via upload

--- 0508_nimblrTA_strategy.R
+++ 0508_nimblrTA_strategy.R
@@ -31,7 +31,7 @@ a01 <-  BatchGetSymbols(
   tickers = cntrt02$SYMBOL02,
   first.date = Sys.Date() - 3600,
   last.date = Sys.Date(),
-  thresh.bad.data = 0.75,
+  thresh.bad.data = 0.2,
   bench.ticker = "^NSEI",
   type.return = "arit",
   freq.data = "daily",
@@ -46,3 +46,79 @@ a01 <-  BatchGetSymbols(
 
 a01con <- data.table(a01$df.control)
 a02 <- data.table(a01$df.tickers)
+
+#
+# Extract the Nifty50 data
+#
+
+a01nfity50 <-  BatchGetSymbols(
+  tickers = "^NSEI",
+  first.date = Sys.Date() - 3600,
+  last.date = Sys.Date(),
+  thresh.bad.data = 0.75,
+  bench.ticker = "^NSEI",
+  type.return = "arit",
+  freq.data = "daily",
+  how.to.aggregate = "last",
+  do.complete.data = FALSE,
+  do.fill.missing.prices = TRUE,
+  do.cache = TRUE,
+  cache.folder = file.path(tempdir(), "BGS_Cache"),
+  do.parallel = TRUE, # FALSE
+  be.quiet = FALSE
+)
+
+a02nfity50 <- data.table(a01nfity50$df.tickers)
+setnames(a02nfity50, paste("NF", names(a02nfity50), sep = "_"))
+
+# Merge the data with remaining data
+
+a03all <- merge(x = a02,
+                y = a02nfity50, 
+                by.x = c("ref.date"),
+                by.y = c("NF_ref.date"))
+
+#
+# Calculate Relative price strength
+# https://corporatefinanceinstitute.com/resources/knowledge/trading-investing/relative-price-strength-rps/
+#
+a03all <- a03all [, rs := price.adjusted / NF_price.adjusted, ]
+a03all <- a03all [, `:=` (rs_sma55 = SMA(rs, 55), rs_ema55 = EMA(rs, 55),
+                          rs_mean55 = runMean(rs, 55),
+                          rs_sd55 = runSD(rs, 55) ), by =.(ticker)]
+
+a03all <- a03all [, `:=` (prc_ema55 = SMA(price.adjusted, 55), 
+                          prc_ema20 = EMA(price.adjusted, 20),
+                          low20 = runMin(price.adjusted, 20),
+                          high55 = runMax(price.adjusted, 55), 
+                          high200 = runMax(price.adjusted, 200) ), by =.(ticker)]
+
+a03all <- a03all [, jdk_rs55 := 100 + ((rs - rs_mean55)/rs_sd55) + 1, ]
+a03all <- a03all [, jdk_roc55 := 100 * (shift(jdk_rs55, type = "lag", n=1) / jdk_rs55 - 1), by = .(ticker)]
+a03all <- a03all [, `:=` (jdk_roc55_mean55 = runMean(jdk_roc55, 55),
+                          jdk_roc55_sd55 = runSD(jdk_roc55, 55) ), by =.(ticker)]
+
+a03all <- a03all [, jdk_momratio55 := 100 + ((jdk_roc55 - jdk_roc55_mean55)/jdk_roc55_sd55) + 1, ]
+
+a04all <- na.omit(a03all)
+a04all <- a04all [, qudrant := case_when(jdk_rs55 > 100 & jdk_momratio55 > 100 ~ 1,
+                                         jdk_rs55 > 100 & jdk_momratio55 < 100 ~ 2,
+                                         jdk_rs55 < 100 & jdk_momratio55 > 100 ~ 3,
+                                         jdk_rs55 < 100 & jdk_momratio55 < 100 ~ 4 ), ]
+
+#################################################################
+#
+# Calculate NibmlrTA
+# http://nimblr.pbworks.com/w/page/124439211/NimblrTA
+#
+# Candle height [CH]
+# Body height [BH]
+#
+# Calculate BH/CH %: if the % is >=50% then breakout candle
+#                    if the % is < 50% then Non-breakout  
+#################################################################
+a04all <- a04all [, `:=`(ch = price.high - price.low,
+                         bh = abs(price.open - price.close) ), ]
+
+a04all <- a04all [, bh_ch_per := round( (bh/ch) * 100, 1), ]
+a04all <- a04all [, bh_ch_cat := ifelse(bh_ch_per >=50, "Breakout", "Non-breakout"), ]
