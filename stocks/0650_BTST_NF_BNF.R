f55db448658fb24a5144c7bf2995573d32778012
coursephd <mahajvi@yahoo.co.in>
2024-09-01 11:32:52 +0530

    Add files via upload

--- stocks/0650_BTST_NF_BNF.R
+++ stocks/0650_BTST_NF_BNF.R
@@ -20,7 +20,7 @@ options(future.rng.onMisuse="ignore")
 future::plan(future::multisession, workers = floor(parallel::detectCores() ))
 
 a01nfity50 <-  yf_get(
-  tickers = c("^BSESN"), # ^NSEI, ^NSEBANK # fno$SYMBOL02,  
+  tickers = c("^NSEI", "^NSEBANK", "^BSESN"), # ^NSEI, ^NSEBANK, ^BSESN # fno$SYMBOL02,  
   first_date = Sys.Date() - 4000, #"2008-01-01", #Sys.Date() - 5000,
   last_date = Sys.Date(),
   thresh_bad_data = 0.75,
@@ -48,7 +48,43 @@ all02 <- all02 [, -c("price_adjusted", "ret_adjusted_prices", "cumret_adjusted_p
 all02 <- all02 [, diff := price_open - prvclose, ]
 all02 <- all02 [, diff := ifelse(is.na(diff), 0, diff), ]
 all02 <- all02 [, diffcum := cumsum(diff), by = .(ticker)]
+all02 <- all02 [, prfloss := ifelse(diff >0, 1, 0), by = .(ticker)]
+all02 <- all02 [, yr := year(trdate), by = .(ticker)]
+all02 <- all02 [, yrpnl := sum(diff), by = .(yr, ticker)]
 
 ggplot(data = all02, aes (y = diffcum, x = nrow) ) +
   geom_line() +
   geom_point()
+
+############################################
+#
+# Create tableua type interactive plots
+#
+############################################
+
+library(GWalkR)
+gwalkr(all02)
+
+
+############################################
+#
+# Get the numbers in terms 
+# % of win / loss
+# Cumulative amount of money possibly earned
+#
+############################################
+
+all03 <- all02 [, .(pfldays = .N), by = .(ticker, prfloss)]
+all03 <- all03 [, tot := sum(pfldays), by = .(ticker)]
+all03 <- all03 [, perc := pfldays / tot, ]
+
+
+all04 <- all02 [, .(pfldays = .N), by = .(yr, ticker, prfloss, yrpnl)]
+all04 <- all04 [, tot := sum(pfldays), by = .(yr, ticker, yrpnl)]
+all04 <- all04 [, perc := pfldays / tot, ]
+all04 <- all04 [, qnty := ifelse(ticker == "^NSEBANK", 450, 1000), ]
+all04 <- all04 [, totmoney := yrpnl * qnty, ]
+all05 <- all04 [ prfloss == 1]
+
+all05 <- all05 [ order (yr, ticker)]
+all05 <- all05 [, cumtotmoney := cumsum(totmoney), by = .(ticker)]
